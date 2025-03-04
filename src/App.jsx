07ad31c1e4f8dc494da125830127c6bac432d8fc
sconex <admin@sconex-testing.dev>
2025-03-04 11:13:07 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Blackjack from './components/Blackjack'
+
+function App() {
+  return (
+    <div className="App">
+      <Blackjack />
+    </div>
+  )
+}
+
+export default App
